03f3e7e5080a3b4eb64c41bd7c923da51fb91e5a
crb331 <camiloribon31@gmail.com>
2024-04-26 18:43:08 -0500

    eliminate eslint

--- src/routes/cart/+layout.svelte
+++ src/routes/cart/+layout.svelte
@@ -1,21 +1,48 @@
 <script lang="ts">
-  import * as Breadcrumb from '$lib/components/ui/breadcrumb/index';
-  import { page } from '$app/stores';
+	import { page } from '$app/stores';
 
-  $: pathnameRoutes = $page.url.pathname.split('/').splice(1);
+	$: pathnameRoutes = $page.url.pathname.split('/').splice(1);
 	$: console.log({ pathnameRoutes });
 </script>
 
-<!-- Step progress -->
-<Breadcrumb.Root class="mt-1 mx-5">
-	<Breadcrumb.List>
-		{#each pathnameRoutes as path}
-			<Breadcrumb.Item>
-				<Breadcrumb.Link href={`/${path}`}>{path}</Breadcrumb.Link>
-			</Breadcrumb.Item>
-			<Breadcrumb.Separator />
-		{/each}
-	</Breadcrumb.List>
-</Breadcrumb.Root>
+<div class="flex justify-center">
+	<div class="flex items-center justify-between gap-2 bg-red-500 w-3/5 h-32 mt-3 px-3 rounded-md">
+		<!-- step 1 -->
+		<div class="flex flex-col items-center">
+			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
+				>1</span
+			>
+			<h3>Shipping Info</h3>
+		</div>
+		<!-- step 2 -->
+		<div class="flex flex-col justify-center items-center">
+			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
+				>1</span
+			>
+			<h3>Shipping Info</h3>
+		</div>
+		<!-- step 3 -->
+		<div class="flex flex-col justify-center items-center">
+			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
+				>1</span
+			>
+			<h3>Shipping Info</h3>
+		</div>
+		<!-- step 4 -->
+		<div class="flex flex-col justify-center items-center">
+			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
+				>1</span
+			>
+			<h3>Shipping Info</h3>
+		</div>
+		<!-- step 5 -->
+		<div class="flex flex-col justify-center items-center">
+			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
+				>1</span
+			>
+			<h3>Shipping Info</h3>
+		</div>
+	</div>
+</div>
 
 <slot />
