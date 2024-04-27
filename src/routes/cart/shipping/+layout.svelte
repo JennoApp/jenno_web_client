c2e2d75302e0e86a473d15e2ce9279e47755b240
crb331 <camiloribon31@gmail.com>
2024-04-26 19:47:42 -0500

    progress bar

--- src/routes/cart/shipping/+layout.svelte
+++ src/routes/cart/shipping/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+  import { Progress } from '$lib/components/ui/progress'
 
 	$: pathnameRoutes = $page.url.pathname.split('/').splice(1);
 	$: console.log({ pathnameRoutes });
 </script>
 
-<div class="flex justify-center">
-	<div class="flex items-center justify-between gap-2 bg-red-500 w-3/5 h-32 mt-3 px-3 rounded-md">
+<div class="flex flex-col justify-center items-center">
+	<div class="flex items-center justify-evenly gap-2 bg-[#252525] w-3/5 h-24 mt-3 px-3 rounded-t-md">
 		<!-- step 1 -->
 		<div class="flex flex-col items-center">
 			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
@@ -28,21 +29,12 @@
 			>
 			<h3>Shipping Info</h3>
 		</div>
-		<!-- step 4 -->
-		<div class="flex flex-col justify-center items-center">
-			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
-				>1</span
-			>
-			<h3>Shipping Info</h3>
-		</div>
-		<!-- step 5 -->
-		<div class="flex flex-col justify-center items-center">
-			<span class="border border-white rounded-full p-1 w-10 h-10 text-center text-lg font-semibold"
-				>1</span
-			>
-			<h3>Shipping Info</h3>
-		</div>
+		
 	</div>
+  
+  <div class="flex items-center px-16 bg-[#252525] w-3/5 h-10 rounded-b-md">
+    <Progress color="" value={50}/>
+  </div>
 </div>
 
 <slot />
