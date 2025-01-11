801463a81e308aea36e18bf562ca819da686ed4f
crb331 <camiloribon31@gmail.com>
2025-01-11 15:25:53 -0500

    change admin/sales 02

--- src/routes/admin/sales/+page.svelte
+++ src/routes/admin/sales/+page.svelte
@@ -237,7 +237,7 @@
 		</div>
 	{/if}
 {:else}
-	<div class="flex flex-col items-center justify-center h-[calc(100vh-56px)] w-full">
+	<div class="flex flex-col items-center justify-center h-full w-full">
 		<iconify-icon icon="mdi:cash" height="5rem" width="5rem" class="text-[#707070] mb-4" />
 
 		<h1 class="text-xl font-semibold text-[#707070] mb-2">{m.admin_sales_nosales_title()}</h1>
