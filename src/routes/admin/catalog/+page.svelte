928e533455c48012000871b6f96d38ea87ff1e37
crb331 <camiloribon31@gmail.com>
2025-02-05 14:17:49 -0500

    create table 11

--- src/routes/admin/catalog/+page.svelte
+++ src/routes/admin/catalog/+page.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 	import { Button } from '$lib/components/ui/button';
-	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
-	import { addTableFilter, addPagination } from 'svelte-headless-table/plugins';
-	import { writable, readable } from 'svelte/store';
-	import * as Table from '$lib/components/ui/table';
-	import Image from '$lib/components/Image.svelte';
-	import { Input } from '$lib/components/ui/input';
+	import { writable } from 'svelte/store';
 	import DataTableActions from './data-table-actions.svelte';
 	import * as m from '$paraglide/messages';
-	import { goto, invalidate } from '$app/navigation';
+	import { goto } from '$app/navigation';
 	import StatusVisibility from '$lib/components/StatusVisibility.svelte';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 
 	export let data: PageServerData;
 
 	let productsStore = writable(data.products || []);
 	let metaStore = writable(data.meta || {});
-	// const currentPage = parseInt(data.meta?.page?.toString() || '1', 10);
 
 	// Obtener url del servidor
 	let serverUrl: string;
@@ -55,130 +48,7 @@
     loadProducts(newPage, 10);
   }
 
-	// onMount(async () => {
-	// 	await loadProducts(1, 10);
-	// });
-
 	$: console.log({ productsData: $productsStore });
-
-	// let table: any;
-	// let columns: any = null;
-	// let headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates;
-	// let filterValue = '';
-
-	// async function buildTable() {
-	// 	table = createTable(productsStore, {
-	// 		filter: addTableFilter({
-	// 			fn: ({ filterValue, value }) => value.toLowerCase().includes(filterValue.toLowerCase())
-	// 		}),
-	// 		page: addPagination({})
-	// 	});
-
-	// 	columns = table.createColumns([
-	// 		table.column({
-	// 			header: `${m.admin_catalog_tableheader_image()}`,
-	// 			accessor: `imgs`,
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			},
-	// 			cell: ({ value }: { value: any }) => {
-	// 				if (Array.isArray(value) && value.length > 0) {
-	// 					return createRender(Image, { url: value[0] });
-	// 				}
-	// 				return '';
-	// 			}
-	// 		}),
-	// 		table.column({
-	// 			accessor: 'productname',
-	// 			header: `${m.admin_catalog_tableheader_name()}`
-	// 		}),
-	// 		table.column({
-	// 			accessor: 'category',
-	// 			header: `${m.admin_catalog_tableheader_category()}`,
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			}
-	// 		}),
-	// 		table.column({
-	// 			accessor: 'price',
-	// 			header: `${m.admin_catalog_tableheader_price()}`,
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			},
-	// 			cell: ({ value }) => {
-	// 				const formatted = new Intl.NumberFormat('en-US', {
-	// 					style: 'currency',
-	// 					currency: 'USD'
-	// 				}).format(value);
-	// 				return formatted;
-	// 			}
-	// 		}),
-	// 		table.column({
-	// 			accessor: 'quantity',
-	// 			header: `${m.admin_catalog_tableheader_quantity()}`,
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			}
-	// 		}),
-	// 		table.column({
-	// 			accessor: 'visibility',
-	// 			header: `Visibilidad`,
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			},
-	// 			cell: ({ value }) => {
-	// 				return createRender(StatusVisibility, { status: value });
-	// 			}
-	// 		}),
-	// 		table.column({
-	// 			accessor: ({ _id }) => _id,
-	// 			header: '',
-	// 			plugins: {
-	// 				filter: {
-	// 					exclude: true
-	// 				}
-	// 			},
-	// 			cell: ({ value }) => {
-	// 				return createRender(DataTableActions, { id: value });
-	// 			}
-	// 		})
-	// 	]);
-
-	// 	({ headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
-	// 		table.createViewModel(columns));
-
-	// 	filterValue = pluginStates.filter;
-	// }
-
-	// $: if (data.products) {
-	// 	buildTable();
-	// }
-
-	// async function changePage(newPage: number) {
-	// 	if (newPage < 1) return;
-	// 	try {
-	// 		let query = new URLSearchParams($page.url.searchParams.toString());
-	// 		query.set('page', newPage.toString());
-	// 		console.log({ newPage });
-
-	// 		await goto(`${$page.url.pathname}?${query.toString()}`, { replaceState: false });
-
-	// 		await loadProducts(newPage);
-	// 		// await buildTable();
-	// 	} catch (error) {
-	// 		console.error('Error al cambiar de página', error);
-	// 	}
-	// }
 </script>
 
 <div class="flex justify-between max-w-full h-20 px-5 m-5 py-6 flex-shrink">
@@ -190,92 +60,8 @@
 	>
 </div>
 
-<!-- {#if data.products.length !== 0}
-	{#if data.sucess === false}
-		<h1>Error al hacer la solicitud</h1>
-	{:else}
-		<div class="flex items-center mx-10 mt-5">
-			<Input
-				class="max-w-sm placeholder:text-[#707070]"
-				placeholder="Filter names..."
-				type="text"
-				bind:value={filterValue}
-			/>
-		</div>
-		<div class="rounded-md border mx-10 my-5">
-			<Table.Root {...$tableAttrs}>
-				<Table.Header>
-					{#each $headerRows as headerRow}
-						<Subscribe rowAttrs={headerRow.attrs()}>
-							<Table.Row>
-								{#each headerRow.cells as cell (cell.id)}
-									<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
-										<Table.Head {...attrs}>
-											<Render of={cell.render()} />
-										</Table.Head>
-									</Subscribe>
-								{/each}
-							</Table.Row>
-						</Subscribe>
-					{/each}
-				</Table.Header>
-				<Table.Body {...$tableBodyAttrs}>
-					{#each $pageRows as row (row.id)}
-						<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-							<Table.Row {...rowAttrs}>
-								{#each row.cells as cell (cell.id)}
-									<Subscribe attrs={cell.attrs()} let:attrs>
-										<Table.Cell {...attrs}>
-											<Render of={cell.render()} />
-										</Table.Cell>
-									</Subscribe>
-								{/each}
-							</Table.Row>
-						</Subscribe>
-					{/each}
-				</Table.Body>
-			</Table.Root>
-		</div>
-		<div class="flex justify-between mx-10">
-			<div class="">
-				<h3 class="text-sm dark:text-[#707070]">
-					items: {data.meta.itemCount} - pages: {data.meta.pageCount}
-				</h3>
-			</div>
-			<div class="flex items-center justify-end space-x-4">
-				<Button
-					class="border-gray-400 dark:border-[#252525]"
-					variant="outline"
-					size="sm"
-					disabled={!data.meta.hasPreviousPage || currentPage <= 1}
-					on:click={() => changePage(Number(currentPage) - 1)}
-				>
-					Anterior
-				</Button>
-				<Button
-					class="border-gray-400 dark:border-[#252525]"
-					variant="outline"
-					size="sm"
-					disabled={!data.meta.hasNextPage}
-					on:click={() => changePage(Number(currentPage) + 1)}
-				>
-					Siguiente
-				</Button>
-			</div>
-		</div>
-	{/if}
-{:else}
-	<div class="flex flex-col items-center justify-center mt-40 w-full">
-		<iconify-icon icon="solar:box-bold" height="5rem" width="5rem" class="text-[#707070] mb-4" />
-		<h1 class="text-xl font-semibold text-[#707070] mb-2">{m.admin_catalog_nocatalog_title()}</h1>
-		<p class="text-lg text-[#707070]">
-			{m.admin_catalog_nocatalog_p()}
-		</p>
-	</div>
-{/if} -->
-
 {#if Array.isArray($productsStore) && $productsStore.length > 0}
-	<div class="overflow-x-auto w-full">
+	<div class="overflow-x-auto w-full m-5">
 		<table class="min-w-full border-collapse text-left text-sm">
 			<thead>
 				<tr class="border-b bg-gray-100 dark:bg-[#202020]">
@@ -335,9 +121,9 @@
 		</table>
 	</div>
 
-	<div class="flex items-center justify-end space-x-4">
+	<div class="flex items-center justify-end space-x-4 mt-2 m-5">
 		<Button
-			class="border-gray-400 dark:border-[#252525]"
+			class="border-gray-400 dark:border-[#252525] dark:hover:bg-[#252525]"
 			variant="outline"
 			size="sm"
 			disabled={!$metaStore.hasPreviousPage}
@@ -346,7 +132,7 @@
 			Anterior
 		</Button>
 		<Button
-			class="border-gray-400 dark:border-[#252525]"
+			class="border-gray-400 dark:border-[#252525] dark:hover:bg-[#252525]"
 			variant="outline"
 			size="sm"
 			disabled={!$metaStore.hasNextPage}
