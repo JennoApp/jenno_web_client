95dbadec254cb4a2aae3356542591140e45b835e
crb331 <camiloribon31@gmail.com>
2025-02-19 16:19:17 -0500

    paymentRoute success upgrade 02

--- src/routes/api/savecart/+server.ts
+++ src/routes/api/savecart/+server.ts
@@ -3,12 +3,17 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const POST: RequestHandler = async ({ request, locals }) => {
   try {
     const cartItems = await request.json();
-
-    locals.cartItems = cartItems;
+    const cartString = JSON.stringify(cartItems);
 
     console.log('Carrito guardado:', cartItems);
 
-    return new Response('Carrito guardado', { status: 200 });
+
+    return new Response('Carrito guardado', {
+      status: 200,
+      headers: {
+        'Set-Cookie': `cartItems=${encodeURIComponent(cartString)}; Path=/; HttpOnly;`
+      }
+    })
   } catch (error) {
     console.error('Error guardando el carrito:', error);
     return new Response('Error al guardar el carrito', { status: 500 });
