f5070b4769146666e98c2393a4ad1e136550753e
crb331 <camiloribon31@gmail.com>
2025-02-19 17:37:01 -0500

    paymentRoute success upgrade 03

--- src/routes/cart/success/+page.server.ts
+++ src/routes/cart/success/+page.server.ts
@@ -1,8 +1,17 @@
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals }) => {
-  const cartItems = locals.cartItems || [];
+export const load: PageServerLoad = async ({ locals, cookies }) => {
+  let cartItems: any
   const user = locals.user || null;
+
+  const cartCookie = cookies.get('cartItems');
+  if (cartCookie) {
+    try {
+      cartItems = JSON.parse(cartCookie);
+    } catch (error) {
+      console.error('Error al parsear la cookie cartItems:', error);
+    }
+  }
 
   console.log("Carrito en el servidor:", cartItems);
   console.log("Usuario en el servidor:", user);
