14938674926f3e88456a661aaa4632cc88013f63
crb331 <camiloribon31@gmail.com>
2026-09-05 15:10:42 -0500

    add limit to 24 products in initial page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,7 +33,7 @@
 		await getServerUrl();
 
 		const country = page.url.searchParams.get('country') || 'Colombia';
-		const limit = 20;
+		const limit = 24;
 		const nextPage = pageload + 1;
 		let endpoint = '';
 
@@ -67,7 +67,7 @@
 		await getServerUrl();
 		// Obtenemos el país desde la URL o usamos 'Colombia' por defecto
 		const country = page.url.searchParams.get('country') || 'Colombia';
-		const limit = 20;
+		const limit = 24;
 
 		// La categoría en este caso es '' (Todos)
 		const categoryParam = '';
